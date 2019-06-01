64ce69a9cb01942c3941fcf184999cab3ac202b0
Alan Turek <alanturek@gmail.com>
2019-06-01 09:59:15 +0200

    First commit

---
+++ src/main/java/com/example/AKTours/AkToursApplication.java
@@ -0,0 +1,13 @@
+package com.example.AKTours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkToursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkToursApplication.class, args);
+	}
+
+}

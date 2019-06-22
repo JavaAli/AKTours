0fa114cf660ecbef5d214280f7c836a4d113aac1
kasiaurbaniec <katarzyna.urbaniec@gmail.com>
2019-06-22 16:43:52 +0200

    checkstyle

--- src/main/java/com/example/AKTours/web/controllers/TripController.java
+++ src/main/java/com/example/AKTours/web/controllers/TripController.java
@@ -11,7 +11,6 @@ import io.swagger.annotations.Api;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,7 +19,6 @@ import java.util.List;
 @RestController
 @Api(value = "Trips Management System")
 @CrossOrigin(origins = "http://localhost:4200")
-
 public class TripController {
     private final TripService tripService;
 

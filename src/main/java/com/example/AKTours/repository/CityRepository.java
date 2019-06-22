90a7de4abdccb02da53b990c349ca06092fc365f
kasiaurbaniec <katarzyna.urbaniec@gmail.com>
2019-06-22 13:57:38 +0200

    add methods to CityRepository, checkstyle

--- src/main/java/com/example/AKTours/repository/CityRepository.java
+++ src/main/java/com/example/AKTours/repository/CityRepository.java
@@ -1,10 +1,20 @@
 package com.example.AKTours.repository;
 
-
 import com.example.AKTours.model.entity.City;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface CityRepository extends CrudRepository<City, Long> {
 
-     City findCityByName(String name);
+    Optional<City> findCityByName(String name);
+
+    List<City> findCitiesByCountryName(String name);
+
+    @Query(value = "SELECT *FROM cities c JOIN countries count ON c.country_id = count.id JOIN continents cont ON count.continent_id = cont.continent_id WHERE cont.continent_name=?1", nativeQuery = true)
+    List<City> findCitiesByContinentName(String name);
 }

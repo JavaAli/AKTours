a37b297c5d4410a05c8d755fdb23be3015333791
kasiaurbaniec <katarzyna.urbaniec@gmail.com>
2019-06-22 16:46:22 +0200

    remove optional from fongByCityName method checkstyle

--- src/main/java/com/example/AKTours/web/service/CityService.java
+++ src/main/java/com/example/AKTours/web/service/CityService.java
@@ -3,12 +3,9 @@ package com.example.AKTours.web.service;
 import com.example.AKTours.model.entity.City;
 import com.example.AKTours.repository.CityRepository;
 import com.example.AKTours.web.exceptions.EntityNotFoundException;
-
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
-
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +13,7 @@ import org.springframework.stereotype.Service;
 @Log4j2
 @Service
 public class CityService {
+
     @Autowired
     private final CityRepository cityRepository;
 
@@ -30,10 +28,9 @@ public class CityService {
         return collect;
     }
 
-    public Optional<City> findCityByName(String name) throws EntityNotFoundException {
+    public City findCityByName(String name) {
         log.info("Invoke CityRepository findCityByName using" + name);
-        return Optional.of(cityRepository.findCityByName(name)
-                .orElseThrow(() -> new EntityNotFoundException("No city found with name " + name)));
+        return cityRepository.findCityByName(name);
     }
 
     public List<City> findCityByCountryName(String name) throws EntityNotFoundException {
--- src/test/java/com/example/AKTours/web/service/CityServiceTest.java
+++ src/test/java/com/example/AKTours/web/service/CityServiceTest.java
@@ -5,9 +5,11 @@ import com.example.AKTours.model.entity.Continent;
 import com.example.AKTours.model.entity.Country;
 import com.example.AKTours.repository.CityRepository;
 import com.example.AKTours.web.exceptions.EntityNotFoundException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -55,15 +57,22 @@ public class CityServiceTest {
 
     @Test
     public void findCityByName() throws EntityNotFoundException {
-        Mockito.when(cityRepository.findCityByName(Mockito.anyString())).thenReturn(Optional.ofNullable(cityOne));
-        Optional<City> resultCity = cityService.findCityByName("SomeName");
-        assertThat(resultCity.get().getName()).isEqualTo("Panama");
+        Mockito.when(cityRepository.findCityByName(Mockito.anyString())).thenReturn(cityOne);
+        City resultCity = cityService.findCityByName("SomeName");
+        assertThat(resultCity.getName()).isEqualTo("Panama");
     }
 
     @Test
     public void findCityByContinentName() throws EntityNotFoundException {
         Mockito.when(cityRepository.findCitiesByContinentName(Mockito.anyString())).thenReturn(cities);
         List<City> resultCity = cityService.findCityByContinentName("Something");
+        assertThat(resultCity.get(0).getName()).isEqualTo("Panama");
+        assertThat(resultCity.size()).isEqualTo(2);
+    }
+    @Test
+    public void findCityByCountryName() throws EntityNotFoundException {
+        Mockito.when(cityRepository.findCitiesByCountryName(Mockito.anyString())).thenReturn(cities);
+        List<City> resultCity = cityService.findCityByCountryName("Something");
         assertThat(resultCity.get(0).getName()).isEqualTo("Panama");
         assertThat(resultCity.size()).isEqualTo(2);
     }

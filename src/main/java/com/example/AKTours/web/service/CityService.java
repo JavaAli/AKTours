02feb1a5bce6a35aac7a35e8cd22f6f4b4c16a43
kasiaurbaniec <katarzyna.urbaniec@gmail.com>
2019-06-22 13:59:59 +0200

    add methods to CityService, add Test to CityService checkstyle

--- src/main/java/com/example/AKTours/web/service/CityService.java
+++ src/main/java/com/example/AKTours/web/service/CityService.java
@@ -2,17 +2,21 @@ package com.example.AKTours.web.service;
 
 import com.example.AKTours.model.entity.City;
 import com.example.AKTours.repository.CityRepository;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Service;
+import com.example.AKTours.web.exceptions.EntityNotFoundException;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Log4j2
 @Service
 public class CityService {
-
+    @Autowired
     private final CityRepository cityRepository;
 
     public CityService(CityRepository cityRepository) {
@@ -20,15 +24,33 @@ public class CityService {
     }
 
     public List<City> findAll() {
-        log.info("Invoke city repisitory findAll ");
+        log.info("Invoke CityRepisitory findAll ");
         List<City> collect = StreamSupport.stream(cityRepository.findAll().spliterator(), false)
                 .collect(Collectors.toList());
         return collect;
-
     }
 
-    public City findCityByName(String name) {
-        log.info("Invoke city repository using" + name);
-        return cityRepository.findCityByName(name);
+    public Optional<City> findCityByName(String name) throws EntityNotFoundException {
+        log.info("Invoke CityRepository findCityByName using" + name);
+        return Optional.of(cityRepository.findCityByName(name)
+                .orElseThrow(() -> new EntityNotFoundException("No city found with name " + name)));
+    }
+
+    public List<City> findCityByCountryName(String name) throws EntityNotFoundException {
+        log.info("Invoke CityRepository findCityByCoutryName using" + name);
+        if (!cityRepository.findCitiesByCountryName(name).isEmpty()) {
+            return cityRepository.findCitiesByCountryName(name);
+        } else {
+            throw new EntityNotFoundException("Not found any cities for country " + name);
+        }
+    }
+
+    public List<City> findCityByContinentName(String name) throws EntityNotFoundException {
+        log.info("Invoke CityRepository findCityByContinentName using" + name);
+        if (!cityRepository.findCitiesByContinentName(name).isEmpty()) {
+            return cityRepository.findCitiesByContinentName(name);
+        } else {
+            throw new EntityNotFoundException("Not found any cities for continent " + name);
+        }
     }
 }

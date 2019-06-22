c88d9f4afc1fbfcf26e36a067e3435c32b08d63d
kasiaurbaniec <katarzyna.urbaniec@gmail.com>
2019-06-22 13:54:15 +0200

    change City json display

--- src/main/java/com/example/AKTours/model/entity/City.java
+++ src/main/java/com/example/AKTours/model/entity/City.java
@@ -1,24 +1,28 @@
 package com.example.AKTours.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Set;
 
-@Entity
-@Table(name = "Cities")
-@Data
 @AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hotels", "airports"})
 @NoArgsConstructor
+@Table(name = "Cities")
 public class City {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name="city_name")
+    @Column(name = "city_name")
     private String name;
 
     @OneToMany(cascade = CascadeType.ALL)
@@ -29,5 +33,13 @@ public class City {
     @JoinColumn(name = "city_id")
     private Set<Airport> airports;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "country_id")
+    private Country country;
 
+
+    public City(String name, Country country) {
+        this.name = name;
+        this.country = country;
+    }
 }

66454f8ff7fa8c9b1dd1fe9d934e895ee5998de1
kasiaurbaniec <katarzyna.urbaniec@gmail.com>
2019-06-22 16:45:01 +0200

    remove optional from findByCityName method checkstyle

--- src/main/java/com/example/AKTours/repository/CityRepository.java
+++ src/main/java/com/example/AKTours/repository/CityRepository.java
@@ -1,20 +1,18 @@
 package com.example.AKTours.repository;
 
 import com.example.AKTours.model.entity.City;
+import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface CityRepository extends CrudRepository<City, Long> {
 
-    Optional<City> findCityByName(String name);
+    public City findCityByName(String name);
 
-    List<City> findCitiesByCountryName(String name);
+    public List<City> findCitiesByCountryName(String name);
 
     @Query(value = "SELECT *FROM cities c JOIN countries count ON c.country_id = count.id JOIN continents cont ON count.continent_id = cont.continent_id WHERE cont.continent_name=?1", nativeQuery = true)
-    List<City> findCitiesByContinentName(String name);
+    public List<City> findCitiesByContinentName(String name);
 }

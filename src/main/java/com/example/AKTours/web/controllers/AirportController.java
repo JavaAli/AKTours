ce3a4b8cd28a49a80717bc36ef05c4c114e5c91b
kasiaurbaniec <katarzyna.urbaniec@gmail.com>
2019-06-30 19:13:27 +0200

    add cross origin

--- src/main/java/com/example/AKTours/web/controllers/AirportController.java
+++ src/main/java/com/example/AKTours/web/controllers/AirportController.java
@@ -10,12 +10,14 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @Log4j2
 @Controller
 @RequestMapping(value = {"/airports"})
